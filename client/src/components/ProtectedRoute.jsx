ca5dafc30819a1c3bb2c1d11fcecec6617d957d3
Mewing cat <168539334+KazutoZ02@users.noreply.github.com>
2026-03-01 00:30:38 +0530

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,9 +1,5 @@
 import { Navigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../context/';
-
-const';
-
-const.d.ts';
+import { useAuth } from '../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, loading } = useAuth();
